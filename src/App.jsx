d3af8baac7cceaa1ade70f84a4c1e1c62c99dcdf
Syed Mansoor Syed Hussain Shah <58980994+qw4hd2@users.noreply.github.com>
2025-02-14 03:55:55 +0500

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -11,23 +10,9 @@ import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
 import './App.css';
 
-// Auto Redirect Component
-const RedirectToHash = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!window.location.hash) {
-      window.location.replace(`${window.location.origin}/#${location.pathname}`);
-    }
-  }, [location]);
-
-  return null;
-};
-
 const App = () => {
   return (
     <Router>
-      <RedirectToHash />
       <div className="container">
         <Header />
         <Routes>
@@ -37,7 +22,6 @@ const App = () => {
           <Route path="/AdPosting" element={<AdPosting />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/SignIn" element={<SignIn />} />
-          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
